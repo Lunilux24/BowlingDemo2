dc8d43c63e5ce3bfc47430fae7d32385758407da
Bassim Beshry <70615080+Lunilux24@users.noreply.github.com>
2025-02-15 17:41:46 -0800

    Ball Movement

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -6,12 +6,17 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform ballAnchor;
+
     private bool isBallLaunched;
     private Rigidbody ballRB;
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
     private void LaunchBall()
@@ -19,6 +24,8 @@ public class BallController : MonoBehaviour
         if (isBallLaunched) return;
 
         isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
